08cd56919094bddc1cd33a5349b8ce0a5a23279b
regimelab <114866071+regimelab@users.noreply.github.com>
2023-01-01 08:44:14 -0800

    Create fgn.py

---
+++ model/fgn.py
@@ -0,0 +1,31 @@
+import numpy as np
+from scipy.linalg import toeplitz
+
+
+def rho(n: int, H: float):
+    
+    param = H*2
+    if n == 0:
+        return 1
+    else:
+        return ((n+1)**param + (n-1)**param - 2*(n**param)) / 2
+
+def fractional_gaussian_covariance(n:int, hurst1:float, hurst2:float, rho:float, sigma1:float=1, sigma2:float=1):
+    
+    w = lambda h: rho * abs(h)**(hurst1 + hurst2)
+    series = w(n-1) - 2*w(n) + w(n+1)
+    series = sigma1 * sigma2 / 2 * series
+
+    return series
+
+def fractional_gaussian_covariance_series(size:int, hurst1:float, hurst2:float, corr:float, sigma1:float=1, sigma2:float=1):
+
+    series = np.ndarray((2*size, 2*size))   
+    series[:size, :size] = toeplitz([rho(i, hurst1) for i in range(size)])   
+    series[-size:, -size:] = toeplitz([rho(i, hurst2) for i in range(size)])
+    
+    for i in range(size):
+        for j in range(size):
+            series[i, size+j] = series[size+j, i] = fractional_gaussian_covariance(i-j, hurst1, hurst2, corr, sigma1, sigma2)
+            
+    return series
